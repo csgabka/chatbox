29714197b42d1eba62948487ec7854dbcb2f0ec0
Gabi <csgabka@gmail.com>
2020-07-15 16:35:31 +0100

    error

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact component={Auth} />
           <Route path="/chat" exact component={Home} />
-          // <Route component={Error} />
+          
         </Switch>
         </div>
       </HashRouter>
