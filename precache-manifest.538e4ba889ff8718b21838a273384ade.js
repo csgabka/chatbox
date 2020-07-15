b7480392b59bba9419349a3aec46752ba95b3fb4
Gabi <csgabka@gmail.com>
2020-07-15 15:13:20 +0100

    Updates

---
+++ precache-manifest.538e4ba889ff8718b21838a273384ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c0bc98157d6b90c7bca6e40567c33e",
+    "url": "/chatbox/index.html"
+  },
+  {
+    "revision": "f563b0022fd97dfb442b",
+    "url": "/chatbox/static/css/main.39d9934c.chunk.css"
+  },
+  {
+    "revision": "a82f44da1b1dfab41675",
+    "url": "/chatbox/static/js/2.6ded21ae.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/chatbox/static/js/2.6ded21ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f563b0022fd97dfb442b",
+    "url": "/chatbox/static/js/main.66ff1259.chunk.js"
+  },
+  {
+    "revision": "ca33bf76f26bd84abc5f",
+    "url": "/chatbox/static/js/runtime-main.349cc507.js"
+  }
+]);
